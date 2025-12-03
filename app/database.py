fcdee7e87e895a07a51c284cd7147717fd22571d
Kepler <83099879+KeplerSP@users.noreply.github.com>
2025-12-03 08:07:54 -0500

    Actualizar-database-mongoAtlas.py

--- app/database.py
+++ app/database.py
@@ -1,11 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+# ❗ Inserta aquí tu cadena de conexión REAL de MongoDB Atlas.
+#   Reemplaza <PASSWORD> por tu contraseña.
+MONGO_URI = "mongodb+srv://leonardocordovag_db_user:IhQJ6UW6NsPa8GZV@cluster0.83cfzur.mongodb.net/streaming_db?retryWrites=true&w=majority&appName=Cluster0"
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-DB_NAME = os.getenv("DB_NAME", "streaming_db")
+DB_NAME = "streaming_db"
 
 client = None
 db = None
@@ -15,7 +14,7 @@ async def connect_to_mongo():
     global client, db
     client = AsyncIOMotorClient(MONGO_URI)
     db = client[DB_NAME]
-    print("📡 Conectado a MongoDB.")
+    print("📡 Conectado a MongoDB Atlas.")
 
 
 async def close_mongo_connection():
